8b629a197be0c196f92fbd96d90830db9f77a0fd
Alejo Gonzalez Pignol <Alejo_G.P@hotmail.com>
2015-01-24 20:58:26 -0300

    Return to it's original branch
    This reverts commit 77984ad8ef381900c11ffec77b7d17fedb9161a9.

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y = matrix()){
-        x <<- y
-        i <<- NULL
-    }
-    get <- function() x
-    setinv <- function(inv) i <<- inv
-    getinv <- function() i
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+
 }
 
 
